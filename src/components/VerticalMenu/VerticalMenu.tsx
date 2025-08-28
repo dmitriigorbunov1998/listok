f4a3a1f36c2c14a1445568b7994ce5b541b9e7e2
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2025-08-28 17:53:11 +0300

    DOC-1: Произведена корректировка стилей в компоненте VerticalMenu

--- src/components/VerticalMenu/VerticalMenu.tsx
+++ src/components/VerticalMenu/VerticalMenu.tsx
@@ -1,11 +1,46 @@
 'use client';
 
+import React from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined, UserOutlined, DashboardOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
 import styles from './VerticalMenu.module.css';
+
+const MENU_ITEMS = [
+    {
+        key: 'dashboard',
+        icon: <DashboardOutlined />,
+        label: 'Дашборд',
+    },
+    {
+        key: 'mail',
+        icon: <MailOutlined />,
+        label: 'Почта',
+    },
+    {
+        key: 'apps',
+        icon: <AppstoreOutlined />,
+        label: 'Приложения',
+    },
+    {
+        key: 'settings',
+        icon: <SettingOutlined />,
+        label: 'Настройки',
+    },
+    {
+        key: 'profile',
+        icon: <UserOutlined />,
+        label: 'Профиль',
+    },
+];
 
 export const VerticalMenu = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.data}>Меню</div>
+        <div className={styles.menuContainer}>
+            <Menu
+                defaultSelectedKeys={['dashboard']}
+                items={MENU_ITEMS}
+                className={styles.menu}
+            />
         </div>
     );
 };
