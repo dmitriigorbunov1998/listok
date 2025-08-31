2aa6bb0c3195d3d4d462838407eb907fb3a709bf
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2025-08-31 19:54:14 +0300

    DEV-1: Добавлены утилиты для преобразования регистров

---
+++ src/utils/changeCamelToSnakeCase.ts
@@ -0,0 +1,20 @@
+/**
+ * Преобразует ключи объекта из snake_case в camelCase
+ */
+
+export const changeCamelToSnakeCase = (obj: any): any => {
+    const snakeCaseObj: any = {};
+
+    Object.keys(obj).forEach(key => {
+        const snakeKey = key.replace(/([A-Z])/g, '_$1').toLowerCase();
+        const value = obj[key];
+
+        if (value !== null && typeof value === 'object') {
+            snakeCaseObj[snakeKey] = changeCamelToSnakeCase(value);
+        } else {
+            snakeCaseObj[snakeKey] = value;
+        }
+    })
+
+    return snakeCaseObj;
+}
---
+++ src/utils/changeSnakeToCamelCase.ts
@@ -0,0 +1,20 @@
+/**
+ * Преобразует ключи объекта из camelCase в snake_case
+ */
+export const changeSnakeToCamelCase = (obj: any): any => {
+    const camelCaseObj: any = {}
+
+    Object.keys(obj).forEach(key => {
+        const camelKey = key.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+        const value = obj[key];
+
+        if (value !== null && typeof value === 'object') {
+            camelCaseObj[camelKey] = changeSnakeToCamelCase(value);
+        } else {
+            camelCaseObj[camelKey] = value;
+        }
+    })
+
+    return camelCaseObj;
+}
+
