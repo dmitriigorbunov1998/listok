eaee88570d712d595f43368561680a9900d52668
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2025-08-31 19:53:46 +0300

    DEV-1: Произведена корректировка отображения пунктов меню

--- src/components/VerticalMenu/VerticalMenu.tsx
+++ src/components/VerticalMenu/VerticalMenu.tsx
@@ -1,46 +1,35 @@
 'use client';
 
 import React from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined, UserOutlined, DashboardOutlined } from '@ant-design/icons';
+import { SettingOutlined, UserOutlined, DashboardOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import styles from './VerticalMenu.module.css';
 
-const MENU_ITEMS = [
-    {
-        key: 'dashboard',
-        icon: <DashboardOutlined />,
-        label: 'Дашборд',
-    },
-    {
-        key: 'mail',
-        icon: <MailOutlined />,
-        label: 'Почта',
-    },
-    {
-        key: 'apps',
-        icon: <AppstoreOutlined />,
-        label: 'Приложения',
-    },
-    {
-        key: 'settings',
-        icon: <SettingOutlined />,
-        label: 'Настройки',
-    },
-    {
-        key: 'profile',
-        icon: <UserOutlined />,
-        label: 'Профиль',
-    },
-];
-
 export const VerticalMenu = () => {
+    const MENU_ITEMS = [
+        {
+            key: 'dashboard',
+            icon: <DashboardOutlined />,
+            label: 'Dashboard',
+        },
+        {
+            key: 'profile',
+            icon: <UserOutlined />,
+            label: 'Профиль',
+        },
+        {
+            key: 'settings',
+            icon: <SettingOutlined />,
+            label: 'Настройки',
+        },
+    ];
+
     return (
         <div className={styles.menuContainer}>
             <Menu
                 defaultSelectedKeys={['dashboard']}
                 items={MENU_ITEMS}
-                className={styles.menu}
-            />
+                className={styles.menu} />
         </div>
     );
 };
