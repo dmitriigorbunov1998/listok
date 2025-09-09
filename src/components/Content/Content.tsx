a60d6dec5022cdb8ae5d7726195da2c53a4b8d37
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2025-09-09 16:42:56 +0300

    LIST-17: Добавлен компонент TaskPage

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,12 +1,14 @@
 import { VerticalMenu } from '@/components/VerticalMenu/VerticalMenu';
 import { TaskCardsWrapper } from '@/components/TaskCardsWrapper/TaskCardsWrapper';
 import styles from './Content.module.css';
+import { TaskPage } from "@/components/TaskPage/TaskPage";
 
 export const Content = () => {
     return (
         <div className={styles.content}>
             <VerticalMenu />
             <TaskCardsWrapper />
+            <TaskPage />
         </div>
     );
 };
