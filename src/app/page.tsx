5dae4736d5edc0a88b6a5e2ea6eee7fe5c6050f7
vadimefimenko <vadik.efimencko@gmail.com>
2025-09-20 15:32:17 +0300

    (LIST-24): Добавил переходы по ссылкам

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
-'use client';
-
-import dynamic from 'next/dynamic';
 import { Content } from '@/components/Content/Content';
 import './index.css';
-import { Route, Routes } from 'react-router';
-
-const BrowserRouter = dynamic(
-    () => import('react-router-dom').then((mod) => mod.BrowserRouter),
-    { ssr: false }
-);
 
 export default function Home() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Content />} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
+    return <Content />;
+}
