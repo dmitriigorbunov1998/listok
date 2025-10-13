75ecb12cdb8ef8d9e001f588eaf6cc379d3df5ac
DmitriiGorbunov1998 <dmitriigorbunov1998@gmail.com>
2025-10-13 15:23:22 +0300

    (LIST-28): Откорректирована визуализация проектов

--- src/components/ProjectsScreen/ProjectsScreen.tsx
+++ src/components/ProjectsScreen/ProjectsScreen.tsx
@@ -50,85 +50,87 @@ export const ProjectsScreen = () => {
     };
 
     return (
-        <div className='projectsScreenContainer'>
+        <div className='projectsScreen'>
             <VerticalMenu />
-            <div className='projectsContainer'>
+            <div className='projectsContainerWrapper'>
                 <div className='projectsTitleRow'>
                     <h2 className='projectsTitle'>Мои проекты</h2>
                     <Button type="primary" icon={<FolderAddOutlined />} >
                         <div className='projectsCreateButtonText'>Создать проект</div>
                     </Button>
                 </div>
-                <div className='projectsDescription'>
-                    <ul>Здесь представлены проекты, к которым у вас есть доступ. Вы можете:
-                        {projectActions.map((action, index) => (
-                            <li key={index}>{action}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className='projectsItems'>
-                    {projects.map((project) => {
-                        return (
-                            <div key={project.id} className='projectItem'>
-                                <div className='projectContainer'>
-                                    <div className='projectElements'>
-                                        <div className='projectLogoRow'>
-                                            <div className='projectLogo'>
-                                                <img
-                                                    src={project.imageUrl}
-                                                    alt={project.name}
-                                                />
+                <div className='projectsContainerCard'>
+                    <div className='projectsDescription'>
+                        <div>Здесь представлены проекты, к которым у вас есть доступ. Вы можете:
+                            {projectActions.map((action, index) => (
+                                <div key={index}>- {action}</div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='projectsItems'>
+                        {projects.map((project) => {
+                            return (
+                                <div key={project.id} className='projectItem'>
+                                    <div className='projectContainer'>
+                                        <div className='projectElements'>
+                                            <div className='projectLogoRow'>
+                                                <div className='projectLogo'>
+                                                    <img
+                                                        src={project.imageUrl}
+                                                        alt={project.name}
+                                                    />
+                                                </div>
+                                                <div className='projectDropdownMenu'>
+                                                    <Dropdown menu={menuProps}>
+                                                        <div className='projectDropdownButton'><MoreOutlined /></div>
+                                                    </Dropdown>
+                                                </div>
                                             </div>
-                                            <div className='projectDropdownMenu'>
-                                                <Dropdown menu={menuProps}>
-                                                    <div className='projectDropdownButton'><MoreOutlined /></div>
-                                                </Dropdown>
+                                            <div className='projectTitle'>
+                                                {project.name} {`(${project.shortName})`}
                                             </div>
-                                        </div>
-                                        <div className='projectTitle'>
-                                            {project.name} {`(${project.shortName})`}
-                                        </div>
-                                        <div className='projectCollaboratorsContainer'>
-                                            <div className='projectUserOutlined'>
-                                                <UserOutlined />
-                                            </div>
-                                            <div className='projectAssigneeUsers'>
-                                                {project.collaboratorsId?.map((collaboratorId)=> {
-                                                    const user = users.find(user => user.id === collaboratorId);
+                                            <div className='projectCollaboratorsContainer'>
+                                                <div className='projectUserOutlined'>
+                                                    <UserOutlined />
+                                                </div>
+                                                <div className='projectAssigneeUsers'>
+                                                    {project.collaboratorsId?.map((collaboratorId)=> {
+                                                        const user = users.find(user => user.id === collaboratorId);
 
-                                                    return (
-                                                        <div className='projectAssigneeUser' key={collaboratorId}>
-                                                            {user?.name}
-                                                        </div>
-                                                    );
-                                                })}
+                                                        return (
+                                                            <div className='projectAssigneeUser' key={collaboratorId}>
+                                                                {user?.name}
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='projectLinkRow'>
-                                            <div className='projectGithubOutlined'>
-                                                <GithubOutlined />
+                                            <div className='projectLinkRow'>
+                                                <div className='projectGithubOutlined'>
+                                                    <GithubOutlined />
+                                                </div>
+                                                <a
+                                                    href={project.githubUrl}
+                                                    target='_blank'
+                                                    className='projectLink'
+                                                >
+                                                    {project.githubUrl}
+                                                </a>
                                             </div>
-                                            <a
-                                                href={project.githubUrl}
-                                                target='_blank'
-                                                className='projectLink'
-                                            >
-                                                {project.githubUrl}
-                                            </a>
-                                        </div>
-                                        <div className='projectStatusRow'>
-                                            <div className='projectDate'>
-                                                {formatDateTime(project.createdAt)}
-                                            </div>
-                                            <div className='projectStatus'>
-                                                <ProjectCardStatus status={project.status} />
+                                            <div className='projectStatusRow'>
+                                                <div className='projectDate'>
+                                                    {formatDateTime(project.createdAt)}
+                                                </div>
+                                                <div className='projectStatus'>
+                                                    <ProjectCardStatus status={project.status} />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
